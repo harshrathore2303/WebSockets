b085229ac02e53c118f15c8449070594ba7849a9
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-06-02 22:13:39 +0530

    socket.io implementation

---
+++ socket.io/client/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import {io} from "socket.io-client"
+import {Container, Typography, TextField, Button} from '@mui/material'
+
+const App = () => {
+  const socket = io("http://localhost:3000/");
+  
+  const [message, setMessage] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected: ", socket.id);
+    });
+
+    socket.on("welcome", (s) => {
+      console.log(s)
+    });
+
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
+  return (
+    <Container maxWidth="sm">
+      <Typography variant='h1' component="div" gutterBottom>
+        Welcome to Socket.io
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+        <TextField value={message} onChange={e => setMessage(e.target.value)} id="outlined-basic" label="Outlined" variant="outlined"></TextField>
+        <Button variant="contained" color="primary">Send</Button>
+      </form>
+    </Container>
+  )
+}
+
+export default App
---
+++ socket.io/server/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+const port = 3000;
+
+const app = express();
+// start by creating server
+const server = createServer(app);
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+//creating a instance of server
+const io = new Server(server, {
+    cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("User Joined");
+  console.log("Id:", socket.id);
+//   socket.emit("welcome", `Welcome to the server ${socket.id}`);
+//   socket.broadcast.emit("welcome", `${socket.id} joined the server`);
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
+
+/*
+    * after doing above step who will notice that "User connected" and "Id" will print 2 times after reloading. Its because of <StrictMode/> in react
+    * io.on send message to all sockets
+*/
+
+//uses the same instance as createServer uses therefore there is no point in using this app.listen after create server with http and using with io.
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+
+server.listen(port, () => {
+    console.log(`Server started in port ${port}`);
+})
